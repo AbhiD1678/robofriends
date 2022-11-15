f7a60e52d9c1f660c65e6b2ffdea5afde3f7bc56
AbhiD1678 <avdubey_b21@ee.vjti.ac.in>
2022-11-15 16:16:15 +0530

    Updates

---
+++ precache-manifest.cafc05c6d579fef1469bfb044caeaa65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7173e623cc45c896ba",
+    "url": "/robofriends/static/css/main.fc0f6e46.chunk.css"
+  },
+  {
+    "revision": "ee7173e623cc45c896ba",
+    "url": "/robofriends/static/js/main.ee7173e6.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "85887cceb2addf74de91",
+    "url": "/robofriends/static/css/2.2e6dfbf8.chunk.css"
+  },
+  {
+    "revision": "85887cceb2addf74de91",
+    "url": "/robofriends/static/js/2.85887cce.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3f192bd747e4d3c6214d479082353ec7",
+    "url": "/robofriends/index.html"
+  }
+];
